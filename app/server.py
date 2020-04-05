281d6559f1628366b434312aa8deda19c0e95dfa
Serigne Saliou Gueye <gueyesaliou067@gmail.com>
2020-04-05 20:34:09 +0200

    fin de la mise en plce du serveur :statut:done

--- app/server.py
+++ app/server.py
@@ -52,7 +52,10 @@ def get_dashboard():
 # port = int(os.environ.get('PORT', 5000))
 # ...
 # app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=port, debug=True)
     # app.run(host = '0.0.0.0' ,port = 5000 )
