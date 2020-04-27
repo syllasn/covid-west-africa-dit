38e9223ba808f79eb27c4720cddd6e453d1e62a7
Serigne Saliou Gueye <gueyesaliou067@gmail.com>
2020-04-28 01:13:33 +0200

    affichage par cas

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 from flask import Flask ,jsonify,request, send_from_directory,render_template, Response, send_file, make_response
 from app import Covid19
 import sys
+import pandas as pd 
+
 from flask_cors import CORS
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from io import BytesIO
@@ -15,6 +17,7 @@ import random
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.dates import DateFormatter
+from graph_code import Graph
 
 
 import json
@@ -23,15 +26,86 @@ import base64
 app = Flask(__name__)
 CORS(app)
 dataframe = Covid19()
+graph = Graph()
 
 
-@app.route('/graph', methods=['GET'])
+@app.route('/graph', methods=['POST'])
 def get_dashboard():
-    bar=dataframe.covid('Covid19SN_datas.xlsx')
+    # bar=dataframe.covid('Covid19SN_datas.xlsx')
+    file_name = 'Covid19SN_datas.xlsx' 
+    df = pd.read_excel(file_name, index_col=0)
+    df.shape
+    df = df.reset_index()
+    values = request.json
+    print(values)
+    if not values :
+        response = {
+            'message':'no data found' 
+
+        }
+        return jsonify(response)  ,400
+    require_values = ['date_deb','date_fin','option']
+    if not all( field in values for field in require_values):
+        response = {
+            'message': 'Required data is missing'
+        }
+        return jsonify(response)  ,400
+    #url = values['date_deb','date_fin','option']
+    date_deb = values['date_deb']
+    print(date_deb)
+    date_fin = values['date_fin']
+    print(date_fin)
+    option = values['option']
+     #option_1='Confirme', option_2='Contact', option_3='Importe', option_4='Communautaire',option_5='Recovered', option_6='Dead'
+    if(option=='Confirme'):
+        option_1 = option
+        option_2='a'
+        option_3='b'
+        option_4 ='cc '
+        option_5='dd'
+        option_6='dd'
+    elif(otpion=='Contact'):
+        option_2=option
+        option_1='a'
+        option_3='b'
+        option_4 ='cc '
+        option_5='dd'
+        option_6='dd'
+    elif(option=='Importe'):
+        option_3=option
+        option_2='a'
+        option_1='b'
+        option_4 ='cc '
+        option_5='dd'
+        option_6='dd'
+    elif(option=='Communautaire'):
+        option_4=option
+        option_2='a'
+        option_3='b'
+        option_1 ='cc '
+        option_5='dd'
+        option_6='dd'
+    elif(option=='Recovered'):
+        option_5=option
+        option_2='a'
+        option_3='b'
+        option_4 ='cc '
+        option_1='dd'
+        option_6='dd'
+    elif(option=='Dead'):
+        option_6=option
+        option_2='a'
+        option_3='b'
+        option_4 ='cc '
+        option_5='dd'
+        option_1='dd'
+    print(option)
+    data = graph.graphe_sn('Senegal',df,date_deb =date_deb , date_fin=date_fin ,option_1=option_1,option_2=option_2,option_3=option_3,option_4=option_4,option_5=option_5,option_6=option_6)
+    print(data)
     response = {
-        'data': bar
+        'data': data
     }
-    return jsonify(response)  ,200
+    return jsonify(response)  , 200
     # canvas = FigureCanvasAgg(fig)  
     # response= Response( mimetype='image/png') 
     # canvas.print_png(response)
